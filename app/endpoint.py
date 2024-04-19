60f59cc7801dd829a6a517e5d5eb0720e9c22b1b
safwanshamsir99 <safwanshamsir7177@gmail.com>
2024-04-19 16:40:24 +0800

    feat: dev endpoint

---
+++ app/endpoint.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI, status, APIRouter
+from pydantic import BaseModel
+from app.component_module.table import write_table
+from app.component_module.viz import draw_chart
+import pandas as pd
+
+# class Crosstabs(BaseModel):
+#     df: pd.DataFrame 
+#     demos: list[str] 
+#     wise: str
+#     q_ls: list[str] 
+#     multi: list[str] 
+#     name_sort: list[str] 
+#     weight: str
+#     col_seqs: dict
+
+# class Charts(BaseModel):
+#     dfs: list[pd.DataFrame]
+#     sheet_names: list
+
+description = """
+This is a crosstabs generator API from crosstabs-generator-v3.
+"""
+
+app = FastAPI(
+    title="crosstabs generator",
+    description=description,
+    license_info={
+        "name": "MIT",
+        "identifier": "MIT"
+    }
+)
+
+router = APIRouter(prefix="/crossart")
+
+@router.get("/", status_code=status.HTTP_200_OK, tags=["test"])
+def root():
+    return {"status": "ok",
+            "type": "crosstabsgen"}
+
+# @router.post("/crosstabs", tags=["crosstabsgen"])
+# async def generate_crosstabs(crosstabs: Crosstabs):
+#     '''
+#     Endpoint to generate crosstabs based on the weighted survey file.
+
+#     Request:
+#         - df: pandas DataFrame 
+#         - demos: List of name of the selected demography columns. 
+#         - wise: User selection of the value options. 
+#         - q_ls: List of question column. 
+#         - multi: List of column that contains multiple answer option.
+#         - name_sort: List of column to sort by the name. 
+#         - weight: Name of the selected weight column [str]
+#         - col_seqs:
+#             - Key: Demography column
+#             - Value: Sorted unique value of the key demography column.
+    
+#     Return:
+#         - df_xlsx: conversion result of pandas ExcelWriter that contains crosstabs table into bytes.
+#     '''
+#     pass # will continue later
+
+app.include_router(router)
--- tests/endpoint_test.py
+++ tests/endpoint_test.py
@@ -1,2 +1,9 @@
 from fastapi.testclient import TestClient
+from app.endpoint import app
 
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/crossart/")
+    assert response.status_code == 200, "Response 404, failed"
+    assert response.json() == {"status": "ok", "type": "crosstabsgen"}, "No response"
